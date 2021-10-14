b1cdcc881ef675c710786dab5ad270e47eface02
Isabel Muñoz <ishamu023@gmail.com>
2021-10-14 14:53:08 -0300

    ciclo de vida

---
+++ app/src/main/java/com/example/ciclodevida/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.ciclodevida;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+        private final String TAG="MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        guardarLog("onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        guardarLog("onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        guardarLog("onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        guardarLog("onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        guardarLog("onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        guardarLog("onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        guardarLog("onRestart");
+    }
+
+    private void guardarLog(String nombreFuncion){
+        Log.d(TAG,"Estoy en la función "+nombreFuncion);
+    }
+}
